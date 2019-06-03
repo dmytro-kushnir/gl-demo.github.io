422769cd6ac3ab72adc47edaeadef2551429b401
Dmytro <dimakush1@gmail.com>
2019-06-03 10:21:15 +0300

    Updates

---
+++ precache-manifest.fff1a5e6c09696f1e20397a9134062fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb1778b8401510ee844bfcc8295075d",
+    "url": "/gl-demo.github.io/index.html"
+  },
+  {
+    "revision": "7cfb6fb1eceaf1a30fa2",
+    "url": "/gl-demo.github.io/static/css/main.f8d8a223.chunk.css"
+  },
+  {
+    "revision": "a611f5ed79b793853149",
+    "url": "/gl-demo.github.io/static/js/2.ea066275.chunk.js"
+  },
+  {
+    "revision": "7cfb6fb1eceaf1a30fa2",
+    "url": "/gl-demo.github.io/static/js/main.354aeb67.chunk.js"
+  },
+  {
+    "revision": "16997537009993042ef9",
+    "url": "/gl-demo.github.io/static/js/runtime~main.1ff7a3dc.js"
+  }
+]);
